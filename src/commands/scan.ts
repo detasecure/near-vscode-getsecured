7de9e3a3e0419a6cf7b1c56d7d2d82ef3013a23e
Tamaghna Basu <tamaghna.basu@gmail.com>
2023-09-29 20:28:45 +0400

    Added scanning in progress message and no issues found message

--- src/commands/scan.ts
+++ src/commands/scan.ts
@@ -5,7 +5,6 @@ import { addToContext, getFromContext } from '../extension';
 import axios from 'axios';
 
 export const scanCode = async (context: vscode.ExtensionContext, localWorkspace: string) => {
-
     // Get the active text editor
     const editor = vscode.window.activeTextEditor;
 
@@ -14,56 +13,62 @@ export const scanCode = async (context: vscode.ExtensionContext, localWorkspace:
         const document = editor.document;
         const sourceCode = document.getText();
 
-        // Perform POST request to your API
-        try {
-            // const response = await axios.post('http://localhost:8080/scan_code', {
-            //     source_code: sourceCode
-            // });
+        // Show loading message while scanning
+        vscode.window.withProgress({
+            location: vscode.ProgressLocation.Notification,
+            title: "Scanning code...",
+            cancellable: false
+        }, async (progress) => {
+            try {
+                const response = await axios.post('http://localhost:8080/scan_code', {
+                    source_code: sourceCode
+                });
 
-            const response = await axios.post('http://3.108.126.225:8080/scan_code', {
-                source_code: sourceCode
-            });
+                // const response = await axios.post('http://3.108.126.225:8080/scan_code', {
+                //     source_code: sourceCode
+                // });
 
-            // Display the response in a new window
-            const panel = vscode.window.createWebviewPanel(
-                'scanResult',
-                'Scan Result V1',
-                vscode.ViewColumn.Two,
-                {}
-            );
+                
+                // Display the response in a new window
+                const panel = vscode.window.createWebviewPanel(
+                    'scanResult',
+                    'Scan Result V1',
+                    vscode.ViewColumn.Two,
+                    {}
+                );
 
-            // Display the response in a new window
-            const panel2 = vscode.window.createWebviewPanel(
-                'scanResult',
-                'Scan Result V2',
-                vscode.ViewColumn.Two,
-                {}
-            );
+                const panel2 = vscode.window.createWebviewPanel(
+                    'scanResult',
+                    'Scan Result V2',
+                    vscode.ViewColumn.Two,
+                    {}
+                );
 
-            let findingsTable = generateReportHTML(response.data.findings);
-            let findingsTable2 = generateReportHTMLVersion2(response.data.findings);
+                let findingsTable = generateReportHTML(response.data.findings);
+                let findingsTable2 = generateReportHTMLVersion2(response.data.findings);
 
-            panel.webview.html = findingsTable;
-            panel2.webview.html = findingsTable2;
+                panel.webview.html = findingsTable;
+                panel2.webview.html = findingsTable2;
 
-        } catch (error) {
-            vscode.window.showErrorMessage(`Error scanning code: ${error}`);
-        }
+            } catch (error) {
+                vscode.window.showErrorMessage(`Error scanning code: ${error}`);
+            }
+        });
     } else {
         vscode.window.showErrorMessage('No active editor found.');
     }
 };
 
-function exportFindingsToJSON(findings: any) {
-    const data = JSON.stringify(findings, null, 2);
-    const filePath = vscode.workspace.rootPath + '/findings.json';
-    require('fs').writeFileSync(filePath, data);
-    vscode.window.showInformationMessage('Findings exported to ' + filePath);
-}
-
 
 function generateReportHTML(findings: any) {
-  let findingsTable = `
+
+    // Check for the specific condition
+    if (findings.length === 1 && findings[0].issueDescription === "No issue found!") {
+        return `<h1>${findings[0].issueDescription}</h1>`;
+    }
+
+
+    let findingsTable = `
                 <style>
                     table {
                         width: 100%;
@@ -138,20 +143,27 @@ function generateReportHTML(findings: any) {
 }
 
 function generateReportHTMLVersion2(findings: any) {
- // Generate styled HTML for findings
-  const findingsHtml = findings.map((finding: { fileName: any; issueDescription: any; issuePriority: any; CWE: any, lineNumbers: any[]; vulnerableCodeLines: any; }) => {
-      return `
-          <div class="finding">
-              <p class="description">${finding.issueDescription}</p>
-              <p class="priority">Priority: <span class="${finding.issuePriority}">${finding.issuePriority}</span></p>
-              <p class="description">CWE: ${finding.CWE}</p>
-              <p class="lines">Line Numbers: ${finding.lineNumbers}</p>
-              <p class="recommendations">Affected Lines: ${finding.vulnerableCodeLines}</p>
-          </div>
-      `;
-  }).join('');
 
-  var findingsTable = `
+    // Check for the specific condition
+    if (findings.length === 1 && findings[0].issueDescription === "No issue found!") {
+        return `<h1>${findings[0].issueDescription}</h1>`;
+    }
+    
+
+    // Generate styled HTML for findings
+    const findingsHtml = findings.map((finding: { fileName: any; issueDescription: any; issuePriority: any; CWE: any, lineNumbers: any[]; vulnerableCodeLines: any; }) => {
+        return `
+            <div class="finding">
+                <p class="description">${finding.issueDescription}</p>
+                <p class="priority">Priority: <span class="${finding.issuePriority}">${finding.issuePriority}</span></p>
+                <p class="description">CWE: ${finding.CWE}</p>
+                <p class="lines">Line Numbers: ${finding.lineNumbers}</p>
+                <p class="recommendations">Affected Lines: ${finding.vulnerableCodeLines}</p>
+            </div>
+        `;
+    }).join('');
+
+    var findingsTable = `
                 <html>
                     <head>
                         <style>
@@ -217,6 +229,6 @@ function generateReportHTMLVersion2(findings: any) {
                 </html>
             `;
 
-  return findingsTable;
+    return findingsTable;
 }
 
